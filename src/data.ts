740f01d52bc20b04b69614b96ffd15589023962b
Jeremy Sayers <jeremy1080@gmail.com>
2025-01-23 11:56:04 -0500

    fix times

--- src/data.ts
+++ src/data.ts
@@ -13,7 +13,7 @@ const data: DataItem[] = [
     value: "a",
     title: "Happy Plains",
     text: "The Crimson Gate unlocks the ability to progress through the campaign, do white maps, and outside map activities up to area level 72.",
-    unlockTime: "2025-01-23T23:00:00",
+    unlockTime: "2025-01-23T18:00:00",
     requirments: ["Kill Kitava Solo", "Complete a T5 White Map"],
     incomplete: [
       "Shinichiro",
@@ -32,7 +32,7 @@ const data: DataItem[] = [
     value: "b",
     title: "Electric Slide",
     text: "The Electric Slide gate unlocks the ability to do yellow maps and outsite activities up to area level 78.",
-    unlockTime: "2025-01-23T23:00:00",
+    unlockTime: "2025-01-23T18:00:00",
     requirments: ["Complete a T10 Yellow Map"],
     incomplete: [
       "Shinichiro",
@@ -51,7 +51,7 @@ const data: DataItem[] = [
     value: "c",
     title: "Crimson",
     text: "The Crimson gate unlocks the ability to do red maps and all outsite activities.",
-    unlockTime: "2025-01-26T23:00:00",
+    unlockTime: "2025-01-26T18:00:00",
     requirments: ["Complete a T16 Red Map"],
     incomplete: [
       "Shinichiro",
@@ -70,7 +70,7 @@ const data: DataItem[] = [
     value: "d",
     title: "Dino World",
     text: "The Dino World gate unlocks the ability to take on the first voidstone boss, The Searing Exarch.",
-    unlockTime: "2025-01-27T23:00:00",
+    unlockTime: "2025-01-27T18:00:00",
     requirments: ["Kill the quest version of the Searing Exarch."],
     incomplete: [
       "Shinichiro",
@@ -89,7 +89,7 @@ const data: DataItem[] = [
     value: "e",
     title: "Squid Land",
     text: "The Squid Land gate unlocks the ability to take on the second voidstone boss, The Eater of Worlds.",
-    unlockTime: "2025-01-28T23:00:00",
+    unlockTime: "2025-01-28T18:00:00",
     requirments: ["Kill the quest version of the Eater of Worlds."],
     incomplete: [
       "Shinichiro",
@@ -108,7 +108,7 @@ const data: DataItem[] = [
     value: "f",
     title: "Brain",
     text: "The Brain gate unlocks the abiliity to take on Maven herself directly. (Note that the invitations leading up to her are only gated by the map gates).",
-    unlockTime: "2025-01-29T23:00:00",
+    unlockTime: "2025-01-29T18:00:00",
     requirments: ["Kill the Maven."],
     incomplete: [
       "Shinichiro",
@@ -127,7 +127,7 @@ const data: DataItem[] = [
     value: "g",
     title: "Even Squidlier",
     text: "The Even Squidlier gate allows you to take on the last voidstone boss, Uber Elder. ",
-    unlockTime: "2025-01-30T23:00:00",
+    unlockTime: "2025-01-30T18:00:00",
     requirments: ["Kill Uber Elder"],
     incomplete: [
       "Shinichiro",
